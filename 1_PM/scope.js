d7c32fca46a2b2062eefccc3c42b230941db764f
laurenc8 <laurenc8822@gmail.com>
2021-01-09 23:48:58 -0500

    scope

--- 1_PM/scope.js
+++ 1_PM/scope.js
@@ -1,7 +1,10 @@
 /**
  * Part 1:
  *    What will print and why?
+ *      1 2 undefined
+ *      a is initialized to 1 and printed by console.log(a). Then y defines the variable a = 2 inside the function and prints that value by console.log(a). It then calls x, which defines a variable a but does not set an initial value, so that console.log(a) prints undefined.
  *    What will change if we delete line 15? Why?
+ *      undefined will become 1 because then it prints the global variable a, which equals 1.
  * 
  * Part 2:
  *    Change to ES6 syntax (use arrow functions and change var to let).
@@ -11,17 +14,17 @@
  *        console logs.
  */
 
-function x() {
-  var a;
+const x = () => {
+  let a = 2;
   console.log(a);
 }
 
-function y() {
-  var a = 2;
+const y = () => {
+  let a = 2;
   console.log(a);
   x();
 }
 
-var a = 1;
+let a = 1;
 console.log(a);
 y();
